3b553c1aec017f6d102716685678523c68d663de
ericofrs <ericoqv@gmail.com>
2025-04-15 16:35:09 +0200

    added a protection layer with Bearer auth

--- plumber.R
+++ plumber.R
@@ -1,77 +1,170 @@
 library(plumber)
+library(tidyverse)
+library(usethis)
+source("auth_helper.R")
 library(luxJob)
 
-#* @apiTitle Basic luxJob API
+#* @apiTitle Basic luxJob API with Bearer token authentication
 #* @apiDescription This is a simple API to demonstrate the use of luxJob
 #* @apiVersion 1.0.0
 #* @apiLicense MIT
 
+#* Health check
+#* @get /health
+function() {
+  list(status = "ok")
+  }
+
 #* Echo the parameter that was sent in
 #* @param msg:string  The message to echo back.
 #* @get /echo
-#* @tag echo
-function(msg=""){
-  list(msg = paste0("The message is: '", msg, "'"))
-}
+function(
+    res,
+    req,
+    msg="type your message here"
+)
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      list(msg = paste0("The message is: '", msg, "'"))
+    }
+  )
+  }
 
 #* Get books by id
 #* @param book_id:int  The book id to be selected (1 to 1000)
-#* @get /book
+#* @get /book_recommendations/<book_id>
 #* @tag Books
-function(book_id=5){
-  book_id <- as.integer(book_id)
-  get_book_by_id(book_id)
-}
+function(
+    res,
+    req,
+    book_id=5
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_book_by_id(as.integer(book_id))
+      }
+  )
+  }
 
 #* Get book recommendations
 #* @param skill:str If selected, the skill that it will filter for. (e.g. communication)
 #* @get /book_recommendations
 #* @tag Books
-function(skill=NULL){
-  get_books(skill)
-}
+function(
+    res,
+    req,
+    skill=NULL
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_books(skill)
+    }
+  )
+  }
 
 #* Get company details
 #* @param company_id:int  The company id to be selected.
-#* @get /company_details
+#* @get /companies/<company_id>
 #* @tag Companies
-function(company_id=1){
-  company_id <- as.integer(company_id)
-  get_company_details(company_id)
-}
+function(
+    res,
+    req,
+    company_id=1
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_company_details(as.integer(company_id))
+    }
+  )
+  }
 
 #* Get companies
 #* @param limit:int If selected, it limits the number of rows.
 #* @get /companies
 #* @tag Companies
-function(limit=100){
-  limit <- as.integer(limit)
-  get_companies(limit)
-}
+function(
+    res,
+    req,
+    limit=100
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_companies(as.integer(limit))
+    }
+  )
+  }
 
 #* Get specific learning track
 #* @param track_id:int  The learning track id to be selected.
-#* @get /learning_track_detail
+#* @get /learning_tracks/<track_id>
 #* @tag Learning tracks
-function(track_id){
-  track_id <- as.integer(track_id)
-  get_learning_track_by_id(track_id)
+function(
+    res,
+    req,
+    track_id
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_learning_track_by_id(as.integer(track_id))
+    }
+  )
 }
 
 #* Get learning tracks
 #* @param skill_id:str If selected, it filters for the skill. (url) 
 #* @get /learning_tracks
 #* @tag Learning tracks
-function(skill_id=NULL){
-  get_learning_tracks(skill_id)
+function(
+    res,
+    req,
+    skill_id=NULL
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      get_learning_tracks(skill_id)
+    }
+  )
 }
 
 #* Get specific skill
 #* @param skill_id:str* The skill id to be selected. (url)
-#* @get /selected_skill
+#* @get /skills/<skill_id>
 #* @tag Skills
-function(skill_id){
-  get_skill_by_id(skill_id)
+function(
+    res,
+    req,
+    skill_id
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      skill_id <- utils::URLdecode(skill_id)
+      get_skill_by_id(as.character(skill_id))
+    }
+  )
 }
 
 #* Get skills
@@ -79,18 +172,40 @@ function(skill_id){
 #* @param ordered:str* yes or no. To sort the results alphabetically.
 #* @get /skills
 #* @tag Skills
-function(limit=100, ordered){
-  limit <- as.integer(limit)
-  get_skills(limit, ordered)
+function(
+    res,
+    req,
+    limit=100, ordered
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      limit <- as.integer(limit)
+      get_skills(limit, ordered)
+    }
+  )
 }
 
 #* Get specific vacancy
 #* @param vacancy_id:int  The vacancy id to be selected. (e.g. 917152540)
 #* @get /selected_vacancy
 #* @tag Vacancies
-function(vacancy_id){
-  vacancy_id <- as.integer(vacancy_id)
-  get_vacancy_by_id(vacancy_id)
+function(
+    res,
+    req,
+    vacancy_id
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      vacancy_id <- as.integer(vacancy_id)
+      get_vacancy_by_id(vacancy_id)
+    }
+  )
 }
 
 #* Get vacancies
@@ -100,10 +215,21 @@ function(vacancy_id){
 #' @param limit:int The maximum number of vacancies to return.
 #* @get /vacancies
 #* @tag Vacancies
-function(occupation=NULL, company_id = NULL, canton = NULL, limit = 100){
-  if (!is.null(company_id)) {company_id <- as.integer(company_id)}
-  limit <- as.integer(limit)
-  get_vacancies(occupation, company_id, canton, limit)
+function(
+    res,
+    req,
+    occupation=NULL, company_id = NULL, canton = NULL, limit = 100
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      if (!is.null(company_id)) {company_id <- as.integer(company_id)}
+      limit <- as.integer(limit)
+      get_vacancies(occupation, company_id, canton, limit)
+    }
+  )
 }
 
 #* Get vacancies with skill
@@ -113,10 +239,21 @@ function(occupation=NULL, company_id = NULL, canton = NULL, limit = 100){
 #' @param limit:int The maximum number of vacancies to return.
 #* @get /vacancies_w_skill
 #* @tag Vacancies
-function(skill_id=NULL, company_id = NULL, canton = NULL, limit = 100){
-  if (!is.null(company_id)) {company_id <- as.integer(company_id)}
-  limit <- as.integer(limit)
-  get_vacancies_with_skill(skill_id, company_id, canton, limit)
+function(
+    res,
+    req,
+    skill_id=NULL, company_id = NULL, canton = NULL, limit = 100
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      if (!is.null(company_id)) {company_id <- as.integer(company_id)}
+      limit <- as.integer(limit)
+      get_vacancies_with_skill(skill_id, company_id, canton, limit)
+    }
+  )
 }
 
 #* Log search
@@ -124,7 +261,18 @@ function(skill_id=NULL, company_id = NULL, canton = NULL, limit = 100){
 #* @param query:str* query to be posted in the search log 
 #* @post /log_search
 #* @tag Logs
-function(user_id=NULL, query){
-  if (!is.null(user_id)) {user_id <- as.integer(user_id)}
-  log_search(user_id, query)
+function(
+    res,
+    req,
+    user_id=NULL, query
+    )
+  {
+  auth_helper(
+    res,
+    req,
+    function() {
+      if (!is.null(user_id)) {user_id <- as.integer(user_id)}
+      log_search(user_id, query)
+    }
+  )
 }
--- run_plumber.R
+++ run_plumber.R
@@ -1,2 +1,50 @@
-plumber::pr("plumber.R") |> 
-  plumber::pr_run(port = 8008)
+library(plumber)
+library(luxJob)
+
+add_auth <- function(
+    api,
+    paths = NULL
+) {
+  
+  api[["components"]] <- list(
+    securitySchemes = list(
+      bearerAuth = list(
+        type = "http",
+        scheme = "bearer",
+        description = "Add bearer token here"
+      )
+    )
+  )
+  
+  if (is.null(paths)) paths <- names(api$paths)
+  for (path in paths) {
+    nn <- names(api$paths[[path]])
+    for (p in intersect(nn, c("get", "head", "post", "put", "delete"))) {
+      api$paths[[path]][[p]] <- c(
+        api$paths[[path]][[p]],
+        list(security = list(list(bearerAuth = vector())))
+      )
+    }
+  }
+  
+  api
+  
+}
+
+pr("plumber.R") |> #nolint
+  pr_set_api_spec(add_auth) |>
+  pr_hook("preroute", function(req, res) {
+    res$setHeader("Access-Control-Allow-Origin", "http://localhost:1234")
+    res$setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+    res$setHeader("Access-Control-Allow-Headers", "Authorization, Accept, Content-Type")
+    res$setHeader("Access-Control-Allow-Credentials", "true")
+    if (req$REQUEST_METHOD == "OPTIONS") {
+      res$status <- 200
+      return(list())
+    }
+    plumber::forward()
+  }) |>
+  pr_run(
+    port = 8008,
+    host = "0.0.0.0"
+  )
